32506874f139cdbcf0143280c04c69dda964e4c6
BIGDEV <mahadevraaamesh@gmail.com>
2026-01-10 22:51:08 +0530

    cors change again

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(cors({
   
   origin: [
     'http://localhost:5173',                   
-    'https://campus-shield-ai.web.app/' 
+    'https://campus-shield-ai.web.app' 
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true 
